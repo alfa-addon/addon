033ea4181a32d5b17ba5aa85061d48f1e5f84973
Intel1 <luisriverap@hotmail.com>
2017-07-31 12:07:53 -0500

    downace: nuevo servidor
    Nuevo servidor

---
+++ plugin.video.alfa/servers/downace.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from core import httptools
+from core import logger
+from core import scrapertools
+
+
+def test_video_exists(page_url):
+    logger.info("(page_url='%s')" % page_url)
+    data = httptools.downloadpage(page_url).data
+    if "no longer exists" in data:
+        return False, "[Downace] El fichero ha sido borrado"
+
+    return True, ""
+
+
+def get_video_url(page_url, user="", password="", video_password=""):
+    logger.info("(page_url='%s')" % page_url)
+    data = httptools.downloadpage(page_url).data
+    video_urls = []
+    videourl = scrapertools.find_single_match(data, 'controls preload.*?src="([^"]+)')
+    video_urls.append([".MP4 [downace]", videourl])
+
+    return video_urls
