4ea7f2af2ed29917cf33037df2c30661af6422b4
Intel1 <25161862+Intel11@users.noreply.github.com>
2018-01-26 09:01:16 -0500

    videowood: updated

--- plugin.video.alfa/servers/videowood.py
+++ plugin.video.alfa/servers/videowood.py
@@ -1,34 +1,26 @@
 # -*- coding: utf-8 -*-
 
+from aadecode import decode as aadecode
 from core import scrapertools
+from core import httptools
 from platformcode import logger
 
 
 def test_video_exists(page_url):
     logger.info("(page_url='%s')" % page_url)
-
-    data = scrapertools.cache_page(page_url)
-
+    data = httptools.downloadpage(page_url).data
     if "This video doesn't exist." in data:
-        return False, 'The requested video was not found.'
-
+        return False, '[videowood] El video no puede ser encontrado o ha sido eliminado.'
     return True, ""
 
 
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
     logger.info("url=" + page_url)
     video_urls = []
-
-    data = scrapertools.cache_page(page_url)
+    data = httptools.downloadpage(page_url).data
     text_encode = scrapertools.find_single_match(data, "(eval\(function\(p,a,c,k,e,d.*?)</script>")
-
-    from aadecode import decode as aadecode
     text_decode = aadecode(text_encode)
-
-    # URL del vídeo
     patron = "'([^']+)'"
     media_url = scrapertools.find_single_match(text_decode, patron)
-
     video_urls.append([media_url[-4:] + " [Videowood]", media_url])
-
     return video_urls
