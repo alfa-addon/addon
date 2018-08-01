4c4ddf0b61f24036fc980e6bef1cfaea05651ebc
Intel1 <luisriverap@hotmail.com>
2018-08-01 09:31:05 -0500

    Actualizados
    jawcloud: Nuevo server
    estream: add pattern
    seriesblanco: lenguajes
    qserie: fix
    pelisplusco: fix
    hdfull: fix
    cinemahd: fix

---
+++ plugin.video.alfa/servers/jawcloud.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from core import httptools
+from core import scrapertools
+from platformcode import logger
+
+
+def test_video_exists(page_url):
+    logger.info("(page_url='%s')" % page_url)
+    return True, ""
+
+
+def get_video_url(page_url, user="", password="", video_password=""):
+    logger.info("(page_url='%s')" % page_url)
+    data = httptools.downloadpage(page_url).data
+    video_urls = []
+    videourl = scrapertools.find_single_match(data, 'source src="([^"]+)')
+    video_urls.append([".MP4 [jawcloud]", videourl])
+
+    return video_urls
