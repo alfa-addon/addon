8589bdc118f16d2b584d6dd968f9e1a0f42c3978
Daniel Rincon <daniel.rincon@eurekadms.com>
2019-02-07 17:30:50 -0500

    danimados: eliminados loggers innecesarios

--- plugin.video.alfa/channels/danimados.py
+++ plugin.video.alfa/channels/danimados.py
@@ -168,11 +168,8 @@ def findvideos(item):
     for scrapedserver, scrapeduser in matches:
         data1 = httptools.downloadpage("https://space.danimados.space/gilberto.php?id=%s&sv=mp4" %scrapeduser).data
         data1 = re.sub(r"\n|\r|\t|\s{2}|&nbsp;", "", data1)
-        logger.info("data1111"+data1)
         url = base64.b64decode(scrapertools.find_single_match(data1, '<iframe data-source="([^"]+)"'))
-        logger.info("sadad"+url)
         url1 = devuelve_enlace(url)
-        logger.info("sadadss"+url1)
         if "drive.google" in url1:
             url1 = url1.replace("view","preview")
         if url1:
