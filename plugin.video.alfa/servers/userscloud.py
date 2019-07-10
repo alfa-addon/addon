3fea8a5ad5acca8f25316bc78574295631bb1820
Intel1 <luisriverap@hotmail.com>
2019-07-10 08:33:23 -0500

    Actualizados
    - cinemaupload: Corrección para obtener enlaces.
    - cloudvideo: Patrón corregido.
    - gounlimited: Patrón corregido.
    - netu: Desactivado.
    - okru: test_video_exists actualizado.
    - userscloud: Corrección para obenter enlaces.
    - yourupload: test_video_exists actualizado

--- plugin.video.alfa/servers/userscloud.py
+++ plugin.video.alfa/servers/userscloud.py
@@ -25,13 +25,13 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     packed = scrapertools.find_single_match(data, "function\(p,a,c,k.*?</script>")
     if packed:
         unpacked = jsunpack.unpack(packed)
-    media_url = scrapertools.find_single_match(unpacked, 'src"value="([^"]+)')
+    media_url = scrapertools.find_single_match(unpacked, 'url = "([^"]+)')
     if not media_url:
         id_ = page_url.rsplit("/", 1)[1]
         rand = scrapertools.find_single_match(data, 'name="rand" value="([^"]+)"')
         post = "op=download2&id=%s&rand=%s&referer=%s&method_free=&method_premium=" % (id_, rand, page_url)
-        data = httptools.downloadpage(page_url, post).data
-        media_url = scrapertools.find_single_match(data, '<div id="dl_link".*?<a href="([^"]+)"')
+        data = httptools.downloadpage(page_url, post=post).data
+        media_url = scrapertools.find_single_match(data, 'name="down_script".*?<a href="([^"]+)"')
 
     ext = scrapertools.get_filename_from_url(media_url)[-4:]
     video_urls.append(["%s [userscloud]" % ext, media_url])
