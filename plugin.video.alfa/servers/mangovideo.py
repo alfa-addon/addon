a39e2ff71d0c8c4ab115dcf2b1b391b691f848ff
I7PAEZ\paez <jsaezu@wanadoo.es>
2019-09-12 05:31:04 +0200

    no message

--- plugin.video.alfa/servers/mangovideo.py
+++ plugin.video.alfa/servers/mangovideo.py
@@ -10,7 +10,7 @@ from platformcode import logger
 server = {'1': 'https://www.mangovideo.pw/contents/videos/', '7' : 'https://server9.mangovideo.pw/contents/videos/',
           '8' : 'https://s10.mangovideo.pw/contents/videos/', '9' : 'https://server2.mangovideo.pw/contents/videos/',
           '10' : 'https://server217.mangovideo.pw/contents/videos/', '11' : 'https://234.mangovideo.pw/contents/videos/',
-          '12' : 'https://98.mangovideo.pw/contents/videos/'
+          '12' : 'https://98.mangovideo.pw/contents/videos/', '13' : 'https://68.mangovideo.pw/contents/videos/'
          }
 
 
@@ -29,12 +29,10 @@ def test_video_exists(page_url):
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
     logger.info()
     video_urls = []
-    if "embed" in page_url:
-        data = httptools.downloadpage(page_url)
-        page_url= scrapertools.find_single_match(data, '/(https://mangovideo.pw/get_file/.*?.mp4)/')
-    scrapedurl= scrapertools.find_single_match(page_url, 'https://mangovideo.pw/get_file/\d+/.*?/([^"]+)')
-    scrapedserver = scrapertools.find_single_match(page_url, 'https://mangovideo.pw/get_file/(\d+)')
-    scrapedserver = server.get(scrapedserver, scrapedserver)
-    url = scrapedserver + scrapedurl
+    data = httptools.downloadpage(page_url).data
+    matches = scrapertools.find_multiple_matches(data, 'function/0/https://mangovideo.pw/get_file/(\d+)/\w+/(.*?.mp4)')
+    for scrapedserver,scrapedurl in matches:
+        scrapedserver = server.get(scrapedserver, scrapedserver)
+        url = scrapedserver + scrapedurl
     video_urls.append(["[mangovideo]", url])
     return video_urls
