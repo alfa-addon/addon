d7a7b777a0ea59849bcf71d119fbc35a7d117e9a
shlibidon <39755626+shlibidon@users.noreply.github.com>
2019-09-20 21:16:26 +0200

    powvideo

--- plugin.video.alfa/servers/powvideo.py
+++ plugin.video.alfa/servers/powvideo.py
@@ -15,8 +15,7 @@ headers = [['User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/2
 def test_video_exists(page_url):
     referer = page_url.replace('iframe', 'preview')
 
-    httptools.downloadpage(referer)
-
+    global data
     data = httptools.downloadpage(page_url, headers={'referer': referer}).data
     if data == "File was deleted" or data == '':
         return False, "[powvideo] El video ha sido borrado"
@@ -29,9 +28,6 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     logger.info()
     itemlist = []
 
-    referer = page_url.replace('iframe', 'preview')
-
-    data = httptools.downloadpage(page_url, headers={'referer': referer}).data
     packed = scrapertools.find_single_match(data, "<script type=[\"']text/javascript[\"']>(eval.*?)</script>")
     unpacked = jsunpack.unpack(packed)
     
