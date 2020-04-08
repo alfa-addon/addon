0a1d30279acdde39927c1b3fc506eea2a0102e8a
Alfa-beto <30815244+Alfa-beto@users.noreply.github.com>
2020-04-07 21:27:59 -0300

    servers fixes

--- plugin.video.alfa/servers/supervideo.py
+++ plugin.video.alfa/servers/supervideo.py
@@ -4,15 +4,35 @@ from core import httptools
 from core import scrapertools
 from platformcode import logger
 from lib import jsunpack
+from bs4 import BeautifulSoup
+
+
+def test_video_exists(page_url):
+    global soup
+    logger.info("(page_url='%s')" % page_url)
+
+    data = httptools.downloadpage(page_url).data
+    soup = BeautifulSoup(data, "html5lib")
+    if "File is no longer available as it expired or has been deleted" in data:
+        return False, "[supervideo] El fichero ha sido borrado"
+    return True, ""
 
 
 def get_video_url(page_url, video_password):
     logger.info("(page_url='%s')" % page_url)
     video_urls = []
-    data = httptools.downloadpage(page_url).data
-    packed = scrapertools.find_single_match(data, r'(eval.*?)</script>')
-    unpacked = jsunpack.unpack(packed)
-    url = scrapertools.find_single_match(unpacked, 'file:"([^"]+)"')
-    video_urls.append(["[supervideo]", url])
+    opts = soup.find("ul", class_="dropdown-menu downloadbox__dropdown-menu")
+    matches = opts.find_all("li")
+
+    for elem in matches:
+        dl_url = eval(elem.a["onclick"])
+        qlty = elem.a.span.text.split(",")[0].split("x")[1]
+        dl_data = httptools.downloadpage(dl_url).data
+        url = BeautifulSoup(dl_data, "html5lib").find("a", class_="btn btn_primary btn_direct-download")["href"]
+        video_urls.append(["%s [supervideo]" % qlty, url])
+
     return video_urls
 
+
+def download_video(id, mode, hash):
+    return "https://supervideo.tv/dl?op=download_orig&id=%s&mode=%s&hash=%s" % (id, mode, hash)
