1ca3216e3031468fd18432f228ad6722ceb54929
Intel1 <25161862+Intel11@users.noreply.github.com>
2020-05-26 08:11:56 -0500

    pelismundo
    Obtiene más enlaces

--- plugin.video.alfa/channels/pelismundo.py
+++ plugin.video.alfa/channels/pelismundo.py
@@ -88,6 +88,9 @@ def sub_search(item):
         scrapedtitle = scrapedtitle.replace(st, "")
         st = scrapertools.find_single_match(scrapedtitle, '\(.*?\)')
         scrapedtitle = scrapedtitle.replace(st, "")
+        #hay algunos titulos que tiene doble parentesis
+        st = scrapertools.find_single_match(scrapedtitle, '\(.*?\)')
+        scrapedtitle = scrapedtitle.replace(st, "")
         title = scrapedtitle
         if year:
             title += "(" + year + ")"
@@ -194,11 +197,9 @@ def findvideos(item):
     logger.info()
     itemlist = []
     data = httptools.downloadpage(item.url).data
-    patron = 'SegundaParte.*?ventana-flotante'
-    bloque = scrapertools.find_single_match(data, patron)
-    patron  = 'hand" rel="([^"]+)".*?'
-    patron += 'optxt"><span>([^<]+)</span>.*?'
-    matches = scrapertools.find_multiple_matches(bloque, patron)
+    patron  = '(?is)hand" rel="([^"]+)".*?'
+    patron += 'optxt"><span>([^<]+)</span>'
+    matches = scrapertools.find_multiple_matches(data, patron)
     for scrapedurl, scrapedlanguage in matches:
         if "youtube" in scrapedurl:
             scrapedurl += "&"
