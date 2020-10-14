41818ddb20f50d15b9e321213fe5967bb08d7025
shlibidon <39755626+shlibidon@users.noreply.github.com>
2020-10-14 16:48:06 +0200

    Add files via upload

--- plugin.video.alfa/servers/fembed.py
+++ plugin.video.alfa/servers/fembed.py
@@ -1,35 +1,54 @@
 # -*- coding: utf-8 -*-
+# --------------------------------------------------------
+# Conector videobb By Alfa development Group
+# --------------------------------------------------------
 
 from core import httptools
 from core import scrapertools
-from core import jsontools
 from platformcode import logger
-
-data = ""
 
 
 def test_video_exists(page_url):
-
     global data
     logger.info("(page_url='%s')" % page_url)
-    data = httptools.downloadpage(page_url).data
-    if "Sorry 404 not found" in data or "This video is unavailable" in data or "Sorry this video is unavailable:" in data:
-        return False, "[fembed] El fichero ha sido borrado"
-    page_url = page_url.replace("/f/","/v/")
-    page_url = page_url.replace("/v/","/api/source/")
-    data = httptools.downloadpage(page_url, post={}).json
-
-    if "Video not found or" in data.get('data'):
-        return False, "[Fembed] El fichero ha sido borrado"
+    id = scrapertools.find_single_match("v/(\w+)", page_url)
+    post = "r=&d=videobb.site"
+    data = httptools.downloadpage(page_url, post=post).json
+    if not data["success"]:
+        return False, "[videobb] El video ha sido borrado"
     return True, ""
 
 
 def get_video_url(page_url, user="", password="", video_password=""):
     logger.info("(page_url='%s')" % page_url)
     video_urls = []
-    
-    for videos in data["data"]:
-        v = videos["file"]
-        if not v.startswith("http"): v = "https://www.fembed.com" + videos["file"]
-        video_urls.append([videos["label"] + " [Fembed]", v])
+    for url in data["data"]:
+        video_urls.append([url["label"] + " [videobb]", url["file"]])
+    return video_urls
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------
+# Conector Fembed By Alfa development Group
+# --------------------------------------------------------
+
+from core import httptools
+from core import scrapertools
+from platformcode import logger
+
+
+def test_video_exists(page_url):
+    global data
+    logger.info("(page_url='%s')" % page_url)
+    id = scrapertools.find_single_match("v/(\w+)", page_url)
+    post = "r=&d=feurl.com"
+    data = httptools.downloadpage(page_url, post=post).json
+    if not data["success"]:
+        return False, "[Fembed] El video ha sido borrado"
+    return True, ""
+
+
+def get_video_url(page_url, user="", password="", video_password=""):
+    logger.info("(page_url='%s')" % page_url)
+    video_urls = []
+    for url in data["data"]:
+        video_urls.append([url["label"] + " [Fembed]", url["file"]])
     return video_urls
