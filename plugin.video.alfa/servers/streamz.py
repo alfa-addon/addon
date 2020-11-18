39a2e1b2c86e8d72be1f0b0392ed480ff89162c9
shlibidon <39755626+shlibidon@users.noreply.github.com>
2020-11-18 11:44:49 +0100

    Add files via upload

--- plugin.video.alfa/servers/streamz.py
+++ plugin.video.alfa/servers/streamz.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
+# --------------------------------------------------------
+# Conector Streamz By Alfa development Group
+# --------------------------------------------------------
+
 import re
 from core import httptools
-from core import scrapertools
 from platformcode import logger
-from lib import jsunpack
+
 
 def test_video_exists(page_url):
     global data
     logger.info("(page_url='%s')" % page_url)
-    data = httptools.downloadpage(page_url).data
+    data = httptools.downloadpage(page_url)
     
-    if "File not found, sorry!" in data:
-        return False, "[streamz] El fichero no existe o ha sido borrado"
+    if "<b>File not found, sorry!</b" in data.data:
+        return False, "[Streamz] El fichero no existe o ha sido borrado"
     return True, ""
 
 
@@ -19,16 +22,9 @@ def get_video_url(page_url, video_password):
     logger.info("(page_url='%s')" % page_url)
     video_urls = []
 
-
-    packed = scrapertools.find_single_match(data, r'(eval\(function\(p,a,c,k,e,d\).*?)\s+</script>')
-    unpacked = jsunpack.unpack(packed)
-
-    url = scrapertools.find_single_match(unpacked, '(https://streamz.*?/get.*?.dll)')
-    
-    url = url.replace("getmp4", "getlink").replace("getIink", "getlink")
-    
+    url = re.sub(r'(\.\w{2,3})/\w', '\\1/getlink-', data.url) + '.dll'
     url += "|User-Agent=%s" % httptools.get_user_agent()
-    video_urls.append(["[streamz]", url])
+    video_urls.append([".mp4 [Streamz]", url])
 
     return video_urls
 
