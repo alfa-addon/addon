f28bbfe325a32146ded305753733222320894ba9
Edson Armando <36713890+SistemaRayoXP@users.noreply.github.com>
2021-03-25 23:56:32 -0600

    MediaServer: Corregido servidor no iniciaba si el puerto estaba en uso
    - Ahora se solicita que se ingrese otro puerto (se sugiere 8888, alternativa viable)

--- mediaserver/lib/xbmc.py
+++ mediaserver/lib/xbmc.py
@@ -3,11 +3,32 @@
 # librería que simula xbmc para evitar errores en módulos que lo usen en mediaserver
 # y no tener que poner excepciones en el código del addon
 
+def getCondVisibility(cond):
+    cond = cond.lower()
+    import sys
+    if cond.startswith('system'):
+        if cond.startswith('system.platform'):
+            is_platform = False
+            platform = sys.platform
+            if 'linux' in platform and cond.endswith('linux'):
+                is_platform =  True
+            elif 'darwin' in platform and cond.endswith('osx'):
+                is_platform =  True
+            elif 'win32' in platform and cond.endswith('windows'):
+                is_platform =  True
+    elif cond.startswith('window'):
+        if cond.startswith('window.ismedia'):
+            return False
+    else:
+        return False
+
+
 def getInfoLabel(parm):
     if parm == 'Container.PluginName': return 'plugin.video.alfa'
     elif parm == 'Container.FolderName': return 'Alfa'
 
     return ''
+
 
 class Player(object):
     def __init__(self):
@@ -34,21 +55,19 @@ class Player(object):
     def setSubtitles(subtitleFile):
         return True
 
-def getCondVisibility(cond):
-    cond = cond.lower()
-    import sys
-    if cond.startswith('system'):
-        if cond.startswith('system.platform'):
-            is_platform = False
-            platform = sys.platform
-            if 'linux' in platform and cond.endswith('linux'):
-                is_platform =  True
-            elif 'darwin' in platform and cond.endswith('osx'):
-                is_platform =  True
-            elif 'win32' in platform and cond.endswith('windows'):
-                is_platform =  True
-    elif cond.startswith('window'):
-        if cond.startswith('window.ismedia'):
-            return False
-    else:
-        return False
+
+
+class Monitor(object):
+    class Monitor(object):
+        def waitForAbort(time):
+            milisec = float(time) / 1000
+            time.sleep(milisec)
+
+    def waitForAbort(time):
+        milisec = float(time) / 1000
+        time.sleep(milisec)
+
+
+def sleep(time):
+    milisec = float(time) / 1000
+    time.sleep(milisec)
