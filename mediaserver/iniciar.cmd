ab8d26f29f4727ca40de99d53e30e3d385f3dda0
SistemaRayoXP <edsonarmando78@outlook.com>
2021-05-12 04:10:53 -0500

    MS: El regreso
    - Olvidé el batch

---
+++ mediaserver/iniciar.cmd
@@ -0,0 +1,12 @@
+@echo off
+rem Bypass "Terminate Batch Job" prompt.
+if "%~1"=="-FIXED_CTRL_C" (
+   REM Remove the -FIXED_CTRL_C parameter
+   SHIFT
+) ELSE (
+   REM Run the batch with <NUL and -FIXED_CTRL_C
+   CALL <NUL %0 -FIXED_CTRL_C %*
+   GOTO :EOF
+)
+
+py alfa.py
