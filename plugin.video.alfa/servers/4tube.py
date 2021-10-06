4e0219f710f697f7e7767dc68d8a214174585c3d
paeznet <jsaezu@wanadoo.es>
2021-10-06 18:01:22 +0200

    +18

--- plugin.video.alfa/servers/4tube.py
+++ plugin.video.alfa/servers/4tube.py
@@ -12,16 +12,12 @@ import re
 from core import httptools
 from core import scrapertools
 from platformcode import logger
-import base64
 
 
 def test_video_exists(page_url):
     logger.info("(page_url='%s')" % page_url)
-    global data, server
-    server = scrapertools.find_single_match(page_url, 'www.([A-z0-9-]+).com')
-    data = httptools.downloadpage(page_url).data
-    if "<h2>WE ARE SORRY</h2>" in data or 'Video Not found' in data:
-        return False, "[%s] El fichero no existe o ha sido borrado" % server
+    if "<h2>WE ARE SORRY</h2>" in data or 'Not Found' in data:
+        return False, "[4tube] El fichero no existe o ha sido borrado"
     return True, ""
 
 
@@ -29,21 +25,22 @@ def get_video_url(page_url, video_password):
     logger.info("(page_url='%s')" % page_url)
     video_urls = []
     data = httptools.downloadpage(page_url).data
+    url = scrapertools.find_single_match(data, '<link rel="canonical" href="([^"]+)"')
+    data = httptools.downloadpage(url).data
     data = re.sub(r"\n|\r|\t|&nbsp;|<br>|<br/>", "", data)
-    encode = scrapertools.find_single_match(data,"window.INITIALSTATE =\s+'([^']+)'")
-    decode = base64.b64decode(encode).decode('utf-8')
-    decode = urlparse.unquote(decode)
-    label = scrapertools.find_multiple_matches(decode, '"label":"p(\d+)"')
-    label = label[::-1]
+    data = scrapertools.find_single_match(data, 'id="download-links">(.*?)</div>')
+    id = scrapertools.find_single_match(data, 'data-id="(\d+)"')
+    server = scrapertools.find_single_match(data, 'data-name="([^"]+)"')
+    label = scrapertools.find_multiple_matches(data, 'data-quality="(\d+)"')
+    label.reverse()
     label = '+'.join(label)
-    id = scrapertools.find_single_match(decode,'"mediaId":(\d+),')
     post = {}
-    headers = {"origin":"https://www.%s.com" %server}
-    post_url = "https://token.%s.com/%s/embeds/%s" %(server,id,label)
+    headers = {"origin": "https://www.4tube.com"}
+    post_url = "https://token.%s.com/0000000%s/embeds/%s" %(server,id,label)
     data = httptools.downloadpage(post_url, post = post, headers=headers, add_referer=True).data
     patron = '"(\d+)":.*?'
     patron += ',"token":"([^"]+)"'
     matches = scrapertools.find_multiple_matches(data, patron)
     for quality,url in matches:
-        video_urls.append(["[%s] %sp" % (server,quality), url])
+        video_urls.append(["[4tube] %sp" % quality, url])
     return video_urls
