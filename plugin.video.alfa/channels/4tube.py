bcdce0f1d013251c7e1bd847e20b5d9a61cbd34a
paeznet <jsaezu@wanadoo.es>
2022-08-03 23:16:51 +0200

    +18
    Pelisflix: Cambio dominio
    Asialiveaction: Cambio estructura

--- plugin.video.alfa/channels/4tube.py
+++ plugin.video.alfa/channels/4tube.py
@@ -33,19 +33,19 @@ host = canonical['host'] or canonical['host_alt'][0]
 def mainlist(item):
     logger.info()
     itemlist = []
-    itemlist.append(Item(channel = item.channel, title="Nuevos" , action="lista", url=host + "/videos?sort=date"))
-    itemlist.append(Item(channel = item.channel, title="Popular" , action="lista", url=host + "/videos?time=month"))
-    itemlist.append(Item(channel = item.channel, title="Mas Visto" , action="lista", url=host + "/videos?sort=views&time=month"))
-    itemlist.append(Item(channel = item.channel, title="Mas Valorada" , action="lista", url=host + "/videos?sort=rating&time=month"))
-    itemlist.append(Item(channel = item.channel, title="Longitud" , action="lista", url=host + "/videos?sort=duration&time=month"))
-    itemlist.append(Item(channel = item.channel, title="Pornstars" , action="categorias", url=host + "/pornstars"))
-    itemlist.append(Item(channel = item.channel, title="Canal" , action="categorias", url=host + "/channels"))
-    itemlist.append(Item(channel = item.channel, title="Categorias" , action="categorias", url=host + "/tags"))
-    itemlist.append(Item(channel = item.channel, title="Buscar", action="search", url= host))
+    itemlist.append(item.clone(title="Nuevos" , action="lista", url=host + "/videos?sort=date"))
+    itemlist.append(item.clone(title="Popular" , action="lista", url=host + "/videos?time=month"))
+    itemlist.append(item.clone(title="Mas Visto" , action="lista", url=host + "/videos?sort=views&time=month"))
+    itemlist.append(item.clone(title="Mas Valorada" , action="lista", url=host + "/videos?sort=rating&time=month"))
+    itemlist.append(item.clone(title="Longitud" , action="lista", url=host + "/videos?sort=duration&time=month"))
+    itemlist.append(item.clone(title="Pornstars" , action="categorias", url=host + "/pornstars"))
+    itemlist.append(item.clone(title="Canal" , action="categorias", url=host + "/channels"))
+    itemlist.append(item.clone(title="Categorias" , action="categorias", url=host + "/tags"))
+    itemlist.append(item.clone(title="Buscar", action="search", url= host))
 
     itemlist.append(Item(channel = item.channel, title = ""))
-    itemlist.append(Item(channel = item.channel, title="Trans", action="submenu", orientation="/shemale"))
-    itemlist.append(Item(channel = item.channel, title="Gay", action="submenu", orientation="/gay"))
+    itemlist.append(item.clone(title="Trans", action="submenu", orientation="/shemale"))
+    itemlist.append(item.clone(title="Gay", action="submenu", orientation="/gay"))
     return itemlist
 
 
@@ -53,15 +53,15 @@ def submenu(item):
     logger.info()
     itemlist = []
     url = host + item.orientation
-    itemlist.append(Item(channel = item.channel, title="Nuevos" , action="lista", url=url + "/videos?sort=date"))
-    itemlist.append(Item(channel = item.channel, title="Popular" , action="lista", url=url + "/videos?time=month"))
-    itemlist.append(Item(channel = item.channel, title="Mas Visto" , action="lista", url=url + "/videos?sort=views&time=month"))
-    itemlist.append(Item(channel = item.channel, title="Mas Valorada" , action="lista", url=url + "/videos?sort=rating&time=month"))
-    itemlist.append(Item(channel = item.channel, title="Longitud" , action="lista", url=url + "/videos?sort=duration&time=month"))
-    itemlist.append(Item(channel = item.channel, title="Pornstars" , action="categorias", url=url + "/pornstars"))
-    itemlist.append(Item(channel = item.channel, title="Canal" , action="categorias", url=url + "/channels"))
-    itemlist.append(Item(channel = item.channel, title="Categorias" , action="categorias", url=url + "/tags"))
-    itemlist.append(Item(channel = item.channel, title="Buscar", action="search", url=url))
+    itemlist.append(item.clone(title="Nuevos" , action="lista", url=url + "/videos?sort=date"))
+    itemlist.append(item.clone(title="Popular" , action="lista", url=url + "/videos?time=month"))
+    itemlist.append(item.clone(title="Mas Visto" , action="lista", url=url + "/videos?sort=views&time=month"))
+    itemlist.append(item.clone(title="Mas Valorada" , action="lista", url=url + "/videos?sort=rating&time=month"))
+    itemlist.append(item.clone(title="Longitud" , action="lista", url=url + "/videos?sort=duration&time=month"))
+    itemlist.append(item.clone(title="Pornstars" , action="categorias", url=url + "/pornstars"))
+    itemlist.append(item.clone(title="Canal" , action="categorias", url=url + "/channels"))
+    itemlist.append(item.clone(title="Categorias" , action="categorias", url=url + "/tags"))
+    itemlist.append(item.clone(title="Buscar", action="search", url=url))
     
     return itemlist
 
@@ -93,13 +93,13 @@ def categorias(item):
             title = "%s (%s)" % (title,cantidad.text.strip())
         url =urlparse.urljoin(item.url,url)
         plot = ""
-        itemlist.append(Item(channel = item.channel, action="lista", title=title, url=url,
+        itemlist.append(item.clone(action="lista", title=title, url=url,
                               thumbnail=thumbnail , plot=plot) )
     next_page = soup.find('a', id='next')
     if next_page:
         next_page = next_page['href']
         next_page = urlparse.urljoin(item.url,next_page)
-        itemlist.append(Item(channel = item.channel, action="categorias", title="[COLOR blue]Página Siguiente >>[/COLOR]", url=next_page) )
+        itemlist.append(item.clone(action="categorias", title="[COLOR blue]Página Siguiente >>[/COLOR]", url=next_page) )
     return itemlist
 
 
@@ -139,20 +139,20 @@ def lista(item):
         action = "play"
         if logger.info() == False:
             action = "findvideos"
-        itemlist.append(Item(channel = item.channel, action=action, title=title, url=url, thumbnail=thumbnail,
+        itemlist.append(item.clone(action=action, title=title, url=url, thumbnail=thumbnail,
                                plot=plot, fanart=thumbnail, contentTitle=title ))
     next_page = soup.find('a', id='next')
     if next_page:
         next_page = next_page['href']
         next_page = urlparse.urljoin(item.url,next_page)
-        itemlist.append(Item(channel = item.channel, action="lista", title="[COLOR blue]Página Siguiente >>[/COLOR]", url=next_page) )
+        itemlist.append(item.clone(action="lista", title="[COLOR blue]Página Siguiente >>[/COLOR]", url=next_page) )
     return itemlist
 
 
 def findvideos(item):
     logger.info(item)
     itemlist = []
-    itemlist.append(Item(channel = item.channel, action="play", title= "%s", contentTitle = item.title, url=item.url))
+    itemlist.append(item.clone(action="play", title= "%s", contentTitle = item.title, url=item.url))
     itemlist = servertools.get_servers_itemlist(itemlist, lambda i: i.title % i.server.capitalize())
     return itemlist
 
@@ -160,6 +160,6 @@ def findvideos(item):
 def play(item):
     logger.info(item)
     itemlist = []
-    itemlist.append(Item(channel = item.channel, action="play", title= "%s", contentTitle = item.title, url=item.url))
+    itemlist.append(item.clone(action="play", title= "%s", contentTitle = item.title, url=item.url))
     itemlist = servertools.get_servers_itemlist(itemlist, lambda i: i.title % i.server.capitalize())
     return itemlist
--- plugin.video.alfa/channels/asialiveaction.py
+++ plugin.video.alfa/channels/asialiveaction.py
@@ -30,7 +30,7 @@ canonical = {
             }
 host = canonical['host'] or canonical['host_alt'][0]
 
-IDIOMAS = {'Japones': 'Japones'}
+IDIOMAS = {"Esp": "CAST", "Lat": "LAT", "Sub": "VOSE"}
 list_language = list(IDIOMAS.values())
 list_quality = []
 list_servers = ['gvideo', 'openload','streamango']
@@ -41,39 +41,65 @@ def mainlist(item):
     autoplay.init(item.channel, list_servers, list_quality)
     itemlist = list()
     itemlist.append(Item(channel=item.channel, action="lista", title="Peliculas",
-                             url=urlparse.urljoin(host, "/pelicula"), type='pl'))
+                             url=urlparse.urljoin(host, "/peliculas/"), type='pl'))
     itemlist.append(Item(channel=item.channel, action="lista", title="Series",
-                         url=urlparse.urljoin(host, "/serie"), type='sr'))
-    itemlist.append(Item(channel=item.channel, action="category", title="Géneros", url=host, cat='genre'))
-    itemlist.append(Item(channel=item.channel, action="category", title="Calidad", url=host, cat='quality'))
-    itemlist.append(Item(channel=item.channel, action="category", title="Orden Alfabético", url=host, cat='abc'))
-    itemlist.append(Item(channel=item.channel, action="category", title="Año de Estreno", url=host, cat='year'))
+                         url=urlparse.urljoin(host, "/series-tv/"), type='sr'))
+    itemlist.append(Item(channel=item.channel, action="submenu", title="Idioma",
+                         url=urlparse.urljoin(host, "/peliculas/"), cat='genre'))
+    # itemlist.append(Item(channel=item.channel, action="lista", title="Anime",
+                             # url=urlparse.urljoin(host, "/anime/")))
+    itemlist.append(Item(channel=item.channel, action="category", title="Géneros",
+                         url=urlparse.urljoin(host, "/peliculas/"), cat='genre'))
+    itemlist.append(Item(channel=item.channel, action="category", title="Calidad",
+                         url=urlparse.urljoin(host, "/peliculas/"), cat='quality'))
+    itemlist.append(Item(channel=item.channel, action="category", title="Orden Alfabético",
+                         url=urlparse.urljoin(host, "/peliculas/"), cat='abc'))
+    itemlist.append(Item(channel=item.channel, action="category", title="Año de Estreno",
+                         url=urlparse.urljoin(host, "/peliculas/"), cat='year'))
     itemlist.append(Item(channel=item.channel, title="Buscar", action="search", url=host+"/?s="))
     autoplay.show_option(item.channel, itemlist)
     return itemlist
 
 
+def submenu(item):
+    logger.info()
+    itemlist = list()
+    itemlist.append(Item(channel=item.channel, action="lista", title="Castellano",
+                             url=urlparse.urljoin(host, "/audio-espanol/")))
+    itemlist.append(Item(channel=item.channel, action="lista", title="Latino",
+                             url=urlparse.urljoin(host, "/audio-latino/")))
+    return itemlist
+
+
+
 def category(item):
     logger.info()
     itemlist = list()
-    data = httptools.downloadpage(host, canonical=canonical).data
+    data = httptools.downloadpage(item.url, canonical=canonical).data
     data = re.sub(r"\n|\r|\t|\s{2}|&nbsp;", "", data)
     action = "lista"
-    if item.cat == 'abc':
-        data = scrapertools.find_single_match(data, '<div class="Body Container">(.+?)<main>')
+    if item.cat == 'abc':   
+        data = scrapertools.find_single_match(data, '<ul class="AZList">(.+?)</ul>')
         action = "lista_a"
     elif item.cat == 'genre':
-        data = scrapertools.find_single_match(data, '<a>Géneros<\/a><ul class="sub.menu">(.+?)<a>Año<\/a>')
+        data = scrapertools.find_single_match(data, '>Géneros</div>(.+?)</ul>')
     elif item.cat == 'year':
-        data = scrapertools.find_single_match(data, '<a>Año<\/a><ul class="sub.menu">(.+?)<a>Idioma<\/a>')
+        data = scrapertools.find_single_match(data, '>Año</div>(.+?)</ul>')
     elif item.cat == 'quality':
-        data = scrapertools.find_single_match(data, '<a>Calidad<\/a><ul class="sub-menu">(.+?)<a>Géneros<\/a>')
-    patron = '<li.*?><a href="(.*?)">(.*?)<\/a><\/li>'
+        data = scrapertools.find_single_match(data, '>Calidad</div>(.+?)</ul>')
+    if item.cat == 'abc':
+        patron = '<li><a href="([^"]+)".*?>([^<]+)<' 
+    else:
+        patron = '<li.*?>([^<]+)<a href="([^"]+)"'
     matches = re.compile(patron, re.DOTALL).findall(data)
-    for scrapedurl,scrapedtitle  in matches:
-        if scrapedtitle != 'Próximas Películas':
+    for title,scrapedurl in matches:
+        if item.cat == 'abc':
+            title1=scrapedurl
+            scrapedurl = title
+            title = title1
+        if title != 'Próximas Películas':
             if not scrapedurl.startswith("http"): scrapedurl = host + scrapedurl
-            itemlist.append(item.clone(action=action, title=scrapedtitle, url=scrapedurl, type='cat'))
+            itemlist.append(item.clone(action=action, title=title, url=scrapedurl, type='cat'))
     return itemlist
 
 
@@ -117,14 +143,13 @@ def episodios(item):
     logger.info()
     itemlist = list()
     data = httptools.downloadpage(item.url, canonical=canonical).data
-    data = data.replace('"ep0','"epp"')
-    patron  = '(?is)MvTbImg B.*?href="([^"]+)".*?'
-    patron += 'src="([^"]+)".*?'
-    patron += 'span>Episodio ([^<]+)'
+    patron  = '<a href="([^"]+)" target="_blank" class="fa fa-download".*?'
+    patron += 'Episodio ([^<]+)<'
     matches = scrapertools.find_multiple_matches(data, patron)
-    for scrapedurl, scrapedthumbnail, scrapedepi in matches:
+    for scrapedurl, scrapedepi in matches:
         title="1x%s - %s" % (scrapedepi, item.contentSerieName)
         #urls = scrapertools.find_multiple_matches(scrapedurls, 'href="([^"]+)')
+        scrapedthumbnail = ""
         itemlist.append(item.clone(action='findvideos', title=title, url=scrapedurl, thumbnail=scrapedthumbnail, type=item.type,
                                    infoLabels=item.infoLabels))
     if config.get_videolibrary_support() and len(itemlist) > 0:
@@ -138,53 +163,69 @@ def lista_a(item):
     logger.info()
     itemlist = []
     data = httptools.downloadpage(item.url, canonical=canonical).data
-    patron  = '(?is)Num">.*?href="([^"]+)".*?'
+    patron  = '(?is)MvTbImg">.*?href="([^"]+)".*?'
     patron += 'data-src="([^"]+)".*?>.*?'
     patron += '<strong>([^<]+)<.*?'
-    patron += '<td>([^<]+)<.*?'
+    patron += 'href.*?>([^<]+)<.*?'
     patron += 'href.*?>([^"]+)<\/a>'
     matches = scrapertools.find_multiple_matches(data, patron)
-    for scrapedurl, scrapedthumbnail, scrapedtitle, scrapedyear, scrapedtype in matches:
+    for scrapedurl, scrapedthumbnail, scrapedtitle, scrapedtype, scrapedyear in matches:
         if not scrapedthumbnail.startswith("http"): scrapedthumbnail = "https:" + scrapedthumbnail
-        action = "findvideos"
-        if "Serie" in scrapedtype: action = "episodios"
-        itemlist.append(item.clone(action=action, title=scrapedtitle, contentTitle=scrapedtitle, contentSerieName=scrapedtitle, url=scrapedurl, thumbnail=scrapedthumbnail,
-                       infoLabels={'year':scrapedyear}))
+        new_item = Item(channel=item.channel, title=scrapedtitle, url=scrapedurl, thumbnail=scrapedthumbnail,
+                       type=scrapedtype, infoLabels={'year':scrapedyear})
+        if 'Movie' in scrapedtype:
+            new_item.contentTitle = scrapedtitle
+            new_item.action = 'findvideos'
+        else :
+            new_item.contentSerieName = scrapedtitle
+            new_item.action = 'episodios'
+            
+        itemlist.append(new_item)
+
     tmdb.set_infoLabels_itemlist(itemlist, seekTmdb=True)
+    #pagination
+    url_next_page = scrapertools.find_single_match(data, 'class="next page-numbers" href="([^"]+)"')
+    if len(itemlist)>0 and url_next_page:
+        itemlist.append(item.clone(title="Siguiente >>", url=url_next_page, action='lista_a'))
     return itemlist
 
 
 def lista(item):
     logger.info()
     itemlist = []
-
     data = httptools.downloadpage(item.url, canonical=canonical).data
     data = re.sub(r"\n|\r|\t|\s{2}|&nbsp;", "", data)
-        
     patron  = '(?is)class="TPost C">.*?href="([^"]+)".*?' #scrapedurl
     patron += 'src="([^"]+)".*?>.*?' #scrapedthumbnail
-    patron += 'title">([^<]+)<.*?' #scrapedtitle
-    patron += 'year">([^<]+)<.*?' #scrapedyear
-    patron += 'href.*?>([^"]+)<\/a>' #scrapedtype
+    patron += '(?:T|t)itle">([^<]+)<.*?' #scrapedtitle
+    patron += '(?:Y|y)ear">([^<]+)<.*?' #scrapedyear
+    patron += 'class="Genre">(.*?)</p' #scrapedtype
     matches = scrapertools.find_multiple_matches(data, patron)
     for scrapedurl, scrapedthumbnail, scrapedtitle, scrapedyear, scrapedtype in matches:
         if not scrapedthumbnail.startswith("http"): scrapedthumbnail = "https:" + scrapedthumbnail
         title="%s - %s" % (scrapedtitle,scrapedyear)
-
+        scrapedtype = scrapertools.find_multiple_matches(scrapedtype, 'internal">([^<]+)<')
+        type = scrapedtype[0]
+        language = []
+        for elem in scrapedtype:
+            if "Latino" in elem: lang = "Lat"
+            elif "Español" in elem: lang = "Esp"
+            else: lang = "Sub"
+            language.append(IDIOMAS.get(lang, lang))
         new_item = Item(channel=item.channel, title=title, url=scrapedurl, thumbnail=scrapedthumbnail,
-                       type=scrapedtype, infoLabels={'year':scrapedyear})
-        if scrapedtype == 'Serie':
-            new_item.contentSerieName = scrapedtitle
-            new_item.action = 'episodios'
-        else:
+                       type=scrapedtype, language=language, infoLabels={'year':scrapedyear})
+        if 'Movie' in type:
             new_item.contentTitle = scrapedtitle
             new_item.action = 'findvideos'
+        else :
+            new_item.contentSerieName = scrapedtitle
+            new_item.action = 'episodios'
             
         itemlist.append(new_item)
 
     tmdb.set_infoLabels_itemlist(itemlist, seekTmdb=True)
     #pagination
-    url_next_page = scrapertools.find_single_match(data, 'rel="next" href="([^"]+)"')
+    url_next_page = scrapertools.find_single_match(data, 'class="next page-numbers" href="([^"]+)"')
     if len(itemlist)>0 and url_next_page:
         itemlist.append(item.clone(title="Siguiente >>", url=url_next_page, action='lista'))
     return itemlist
@@ -193,18 +234,21 @@ def lista(item):
 def findvideos(item):
     logger.info()
     itemlist = []
-    data = httptools.downloadpage(item.url, canonical=canonical).data.replace("&quot;",'"').replace("amp;","").replace("#038;","")
-    url = scrapertools.find_single_match(data, '<span><a rel="nofollow" target="_blank" href="([^"]+)"')
-    data = httptools.downloadpage(url).data
-    bloque = scrapertools.find_single_match(data, 'videosJap = \[(.*)\];')
-    matches  = scrapertools.find_multiple_matches(bloque, "(http.*?)'")
-    for url in matches:
-        itemlist.append(item.clone(action = "play", title = "%s", url = url))
+    if not "/paste/" in item.url:
+        data = httptools.downloadpage(item.url, canonical=canonical).data.replace("&quot;",'"').replace("amp;","").replace("#038;","")
+        url = scrapertools.find_single_match(data, '<span><a rel="nofollow" target="_blank" href="([^"]+)"')
+        data = httptools.downloadpage(url).data
+    else:
+        data = httptools.downloadpage(item.url).data
+    matches = scrapertools.find_multiple_matches(data, 'var videos([A-z]{3}) = \[([^<]+)</script>')
+    for lang, bloque in matches:
+        lang = IDIOMAS.get(lang, lang)
+        matches  = scrapertools.find_multiple_matches(bloque, "(http.*?)'")
+        for url in matches:
+            itemlist.append(item.clone(action = "play", title = "%s", language=lang, url = url))
     itemlist = servertools.get_servers_itemlist(itemlist, lambda i: i.title % i.server.capitalize())
-     # Requerido para FilterTools
+    # Requerido para FilterTools
     itemlist = filtertools.get_links(itemlist, item, list_language)
-
     # Requerido para AutoPlay
-
     autoplay.start(itemlist, item)
     return itemlist
