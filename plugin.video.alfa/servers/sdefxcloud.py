157cf50850ed6019807b3ff84d6280f7f4f3a26a
paeznet <jsaezu@wanadoo.es>
2023-02-15 11:56:08 +0100

    +18

--- plugin.video.alfa/servers/sdefxcloud.py
+++ plugin.video.alfa/servers/sdefxcloud.py
@@ -17,27 +17,19 @@ def test_video_exists(page_url):
     return True, ""
 
 
+# https://sdefx.cloud/1pQP10-1Qg >>>>>>  https://dood.la/e/ihnus5jpqc7u8zu8vog3sn70t7zrczds
+
 
 def get_video_url(page_url, video_password):
     logger.info("(page_url='%s')" % page_url)
-    # itemlist = []
     video_urls = []
-    # data = httptools.downloadpage(page_url, headers={'Referer': page_url}).data
     code = scrapertools.find_single_match(data, 'dhYas638H\("([^"]+)"')
     decod = decode(code)
     code = scrapertools.find_single_match(decod, 'dhYas638H\("([^"]+)"')
     decod = decode(code)
     url = scrapertools.find_single_match(decod, '<iframe.*? src="([^"]+)"')
-    logger.debug(url)
     server = servertools.get_server_from_url(url)
     url = servertools.resolve_video_urls_for_playing(server, url)
-    logger.debug(urls)
-    # itemlist = servertools.find_video_items(item.clone(url = url))
-    # itemlist.append(Item(title= "%s", url=url))
-    # itemlist = servertools.get_servers_itemlist(itemlist, lambda i: i.title % i.server.capitalize())
-    # logger.debug(servertools.get_server_from_url(url))
-    # url = servertools.resolve_video_urls_for_playing(url)
-    # logger.debug(url)
     video_urls.append(["[%s]" %server, url])
     return video_urls
 
