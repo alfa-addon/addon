80aa4cf58531dc6665666f3c53156be998c774e5
DieFeM <danifmdelloret@hotmail.com>
2023-08-13 01:40:59 +0200

    Forzar Temporada del titulo
    - Se usa una función para leer el titulo de la pagina y extraer la temporada correcta si esta no coincide con la temporada de la lista. Probablemente esta información podría venir dada en  item.altSeason o lago similar, pero el elemento season recogido en list_all_matches no llega a episodesxseason_matches.
    
    - Limpieza de comentarios innecesarios para el futuro.
    
    - La función clear_title() es ahora get_lang_from_title() ya que se "limpia" por otro lado el título solo se necesita extraer el lenguaje.

--- plugin.video.alfa/channels/henaojara.py
+++ plugin.video.alfa/channels/henaojara.py
@@ -13,17 +13,6 @@ from AlfaChannelHelper import DictionaryAllChannel
 from AlfaChannelHelper import re, traceback, time, base64, xbmcgui
 from AlfaChannelHelper import Item, servertools, scrapertools, jsontools, get_thumb, config, logger, filtertools, autoplay
 
-"""  Esto no es necesario: utiliza AlfaChannel.urlparse o parse_qs para evitar imports innecesarios, ya que Python se puede volver loco por falta de recursos.  No estaban las funciones en AH pero las he añadido.
-if PY3:
-    import urllib.parse as urllib                               # Es muy lento en PY2.  En PY3 es nativo
-    from urllib.parse import urlparse
-    from urllib.parse import parse_qs
-else:
-    import urllib
-    from urlparse import urlparse
-    from urlparse import parse_qs
-"""
-
 IDIOMAS = AlfaChannelHelper.IDIOMAS_ANIME
 list_language = list(set(IDIOMAS.values()))
 list_quality_movies = AlfaChannelHelper.LIST_QUALITY_MOVIES
@@ -32,7 +21,6 @@ list_quality = list_quality_movies + list_quality_tvshow
 list_servers = ['uqload', 'voe', 'streamtape', 'doodstream', 'okru', 'streamlare', 'wolfstream', 'mega']
 forced_proxy_opt = 'ProxySSL'
 
-""" No es necesario para este canal, en principio 'cf_assistant_if_proxy': True, 'cf_assistant_get_source': False, 'CF_stat': True, 'session_verify': False, """
 canonical = {
              'channel': 'henaojara', 
              'host': config.get_setting("current_host", 'henaojara', default=''), 
@@ -53,13 +41,6 @@ url_replace = []
 
 finds = {'find': dict([('find', [{'tag': ['ul'], 'class': ['MovieList']}]), 
                        ('find_all', [{'tag': ['li']}])]), 
-                 """ 'tag' y 'class' son listas y puedes introducir más valores, siempre que luego no coicidan en la misma página.  Por ejemplo, aquí podrías poner:
-                    'class': ['MovieList NoLmtxt Rows AX A06 B04 C03 E20', 'MovieList Rows BX B06 C04 E03 NoLmtxt Episodes']
-                 y no te haría falta poner el código el "list_all".  Pero todavía se puede simplificar más.  En BS4 'class' internamente es tratoado como una lista donde el espacio es un separador, luego en este caso aparece como ['MovieList', 'NoLmtxt', 'Rows', etc...].  Esto quiere decir que siempre que el argunto que pongas en calss sea significativo y no ambiguo, puedes usar solo una entrada y no todo el "chorizo":
-                    'class': ['MovieList'], que te valdría para los dos casos.
-                Si 'MovieList' es ambiguo y tienes que cafificarlo mejor, entonces sí que tienes que pone todo el string.  Ojo que si usas ['MovieList', 'NoLmtxt'] es una función OR mientras que ['MovieList NoLmtxt Rows AX A06 B04 C03 E20'] es AND de todos sus componentes.  Por supuesto también podrías usar 'class': re.compile('MovieList NoLmtxt) o similar para comparar por parte del string.
-                 """: '', 
-         """ Ver línea 135-153 """: '', 
          'categories': dict([('find', [{'tag': ['div'], 'id': ['categories-3']}]),
                              ('find_all', [{'tag': ['li']}])]),
          'search': {}, 
@@ -68,7 +49,6 @@ finds = {'find': dict([('find', [{'tag': ['ul'], 'class': ['MovieList']}]),
          'get_quality': {}, 
          'get_quality_rgx': '', 
          'next_page': {}, 
-         """ Esta web require un '/' al final para el paginado. Hay webs que se ponen muy tontas por esto. """: '', 
          'next_page_rgx': [['\/page\/\d+', '/page/%s/']],
          'last_page': dict([('find', [{'tag': ['div'], 'class': ['wp-pagenavi']}]),
                             ('find_all', [{'tag': ['a'], 'class': ['page-numbers'], '@POS': [-2]}]),
@@ -88,15 +68,12 @@ finds = {'find': dict([('find', [{'tag': ['ul'], 'class': ['MovieList']}]),
          'episode_clean': [], 
          'plot': {}, 
          'findvideos': {'find_all': [{'tag': ['div'], 'class': ['TPlayerTb']}]},
-            """ Tratar de poner aquí todos los regex de limpieza para que AH lo haga luego automáticamente """: '', 
-         'title_clean': [['(?i)HD|Español Castellano|Sub Español|Español Latino|ova\s+\d+\:|OVA\s+\d+|\:|\((.*?)\)|\s19\d{2}|\s20\d{2}', ''],
+         'title_clean': [['(?i)\s*-\s*(Proximo\s*Capitulo\s*\d+-[A-Za-z]+-\d+)', ''],
+                         ['(?i)HD|Español Castellano|Sub Español|Español Latino|ova\s+\d+\:|OVA\s+\d+|\:|\((.*?)\)|\s19\d{2}|\s20\d{2}', ''],
                          ['(?i)\s*Temporada\s*\d+', '']],
          'quality_clean': [],
          'language_clean': [], 
          'url_replace': [], 
-            """ aunque en 'cnt_tot' puedes poner el valor que gustes, es recomendable ajustarse al tamaño habitual de la página en la web. por simple claridad en el debugging.
-            'join_dup_episodes': False evita el intento de AH de reagrupar el mismo episodio con doferentes calidades.  Aquí no hace falta
-            'season_TMDB_limit': False: Nuevo parámetro para que AH no rechaze temporadas que no estén en TMDB.  USAR CON PRECUACIÓN """: '', 
          'controls': {'duplicates': [], 'min_temp': False, 'url_base64': False, 'add_video_to_videolibrary': True, 'cnt_tot': 20, 
                       'get_lang': False, 'reverse': False, 'videolab_status': True, 'tmdb_extended_info': True, 'seasons_search': False, 
                       'IDIOMAS_TMDB': {0: 'es', 1: 'ja', 2: 'es'}, 'join_dup_episodes': False, 'season_TMDB_limit': False}, 
@@ -117,7 +94,6 @@ def mainlist(item):
     itemlist.append(Item(channel=item.channel, title='Últimos Episodios', url=host, action='list_all',
                          thumbnail=get_thumb('new episodes', auto=True), c_type='episodios'))
 
-    """ Esto es una sugenrecia de usabilidad.  Sé que está en Categorías, pero un acceso directo puede venir bien """
     itemlist.append(Item(channel=item.channel, title='Estrenos', url=host + 'ver/category/estrenos/', action='list_all',
                          thumbnail=get_thumb('premieres', auto=True), extra='estrenos'))
 
@@ -127,7 +103,6 @@ def mainlist(item):
     itemlist.append(Item(channel=item.channel, title='Películas', url=host + 'ver/category/pelicula/', action='list_all',
                          thumbnail=get_thumb('movies', auto=True), c_type='peliculas'))
 
-    """ Esto selleva en 'sectio' """
     itemlist.append(Item(channel=item.channel, title='Categorías',  action='section', url=host, 
                          thumbnail=get_thumb('categories', auto=True), extra='categorías'))
 
@@ -140,52 +115,17 @@ def mainlist(item):
 
     return itemlist
 
-""" Esta va en la función 'section' y prácticamente AH lo hace todo automático si se rellena finds[¡categories']
-def categories_menu(item):
-    logger.info()
-
-    new_soup = AlfaChannel.create_soup(item.url, **kwargs)
-    
-    new_soup = new_soup.find(id="categories-3")
-    resultset = new_soup.find_all("li") if new_soup else []
-
-    itemlist = list()
-    
-    for elem in resultset:
-        title = elem.a.text
-        url = elem.a["href"]
-        itemlist.append(Item(channel=item.channel, title=title, url=url, action='list_all',
-                             thumbnail=get_thumb('all', auto=True)))
-
-    return itemlist
-"""
-
 
 def section(item):
     logger.info()
 
     return AlfaChannel.section(item, **kwargs)
 
-""" Pongo al final el formato estándar que Veranime no tenía'
-def newest(categoria):
-    
-    itemlist = []
-    
-    if categoria == 'anime':
-        itemlist = list_all(Item(url=host,c_type='episodios'))
-    return itemlist
-"""
-
 
 def list_all(item):
     logger.info()
 
     findS = finds.copy()
-    """ Ver líneas 56-61
-    if item.c_type == 'episodios':
-        findS['find'] = dict([('find', [{'tag': ['ul'], 'class': ['MovieList Rows BX B06 C04 E03 NoLmtxt Episodes']}]), 
-                              ('find_all', [{'tag': ['li']}])])
-    """
 
     return AlfaChannel.list_all(item, matches_post=list_all_matches, generictools=True, finds=findS, **kwargs)
 
@@ -194,7 +134,7 @@ def list_all_matches(item, matches_int, **AHkwargs):
     logger.info()
 
     matches = []
-    """ Es insteresante dejar esta línea aunque no se use, ya que es solo un puntero y puede ser necesario en el futuro """
+
     findS = AHkwargs.get('finds', finds)
 
     for elem in matches_int:
@@ -202,7 +142,7 @@ def list_all_matches(item, matches_int, **AHkwargs):
         #logger.error(elem)
 
         try:
-            """ usa .get_text(strip=True) en vez de .text.  También puedes usar get_text(''|', strip=True).split('|') y recibes varios textos """
+
             if item.c_type == 'episodios':
                 sxe = elem.find("span", class_="ClB").get_text(strip=True)
                 try:
@@ -214,9 +154,8 @@ def list_all_matches(item, matches_int, **AHkwargs):
                     elem_json['episode'] = 1
                 elem_json['mediatype'] = 'episode'
 
-            """ por estilo porpcura utilizar en lo posible directamente las etiquetas del diccionario elem_json: ahorramos memoria y cada etiqueta significa lo mismo en todos los canales. Eso nos permite también copiar trozos de un canal a otro sin preocuparnos de los nombre """
             elem_json['title'] = elem.find("h3", class_="Title").get_text(strip=True)
-            elem_json['language'], elem_json['title'] = clear_title(elem_json['title'] )
+            elem_json['language'] = get_lang_from_title(elem_json['title'])
 
             seasonPattern = '\s+Temporada\s+(\d+)'
             if re.search(seasonPattern, elem_json['title']):
@@ -224,12 +163,6 @@ def list_all_matches(item, matches_int, **AHkwargs):
                 if season > 1:
                     elem_json['season'] = season
                     elem_json['mediatype'] = 'season'
-            """  Te sugiero que marques con 'temporada' solo los que sean season > 1, por claridad al usuario.  No hace falta que limpies 'title', ya lo hará AH, siempre que hayas puesto el regex en 'finds['title_clean']'
-            Con el truco de las dos líea anteriores, hace la función del elem_json['plot_extend'].  Luego Generictools cambia automáticamente a elem_json['mediatype'] = 'tvshow' después de pintarlo
-                c_title = re.sub(seasonPattern, '', c_title).strip()
-                elem_json['plot_extend'] = '[COLOR blue][Temporada ' + season + '][/COLOR]'
-            elem_json['title'] = c_title
-            """
 
             elem_json['url'] = elem.find("article", class_="TPost C").a.get('href', '')
 
@@ -246,16 +179,9 @@ def list_all_matches(item, matches_int, **AHkwargs):
             if item.c_type == 'peliculas' and elem_json['mediatype'] == 'tvshow':
                 continue
 
-            """ Intentaremos tratar la películas de un solo "episodio" de forma transparente para el usuario, pasando a Findvideos si solo hay 1"""
             if elem_json['mediatype'] == 'movie':
                 elem_json['action'] = 'seasons'
 
-            # logger.info(item.c_type + ', ' + elem_json['mediatype'], True)
-            """ No hace falsta: AH lo hace automáticamente 
-            thumbnail = elem.find("noscript").find("img").get("src", "")
-            if not thumbnail.startswith("https"):
-                thumbnail = "https:%s" % thumbnail
-            """
             elem_json['thumbnail'] = elem.find("noscript").find("img").get("src", "")
 
             try:
@@ -264,7 +190,7 @@ def list_all_matches(item, matches_int, **AHkwargs):
                 elem_json['year'] = '-'
 
             elem_json['quality'] = 'HD'
-            """ elem_json['language'] = lang"""
+
             if elem.find("div", class_=["Description"]): 
                 elem_json['plot'] = elem.find("div", class_=["Description"]).p.get_text(strip=True)
 
@@ -330,14 +256,6 @@ def episodesxseason_matches(item, matches_int, **AHkwargs):
             try:
                 info = elem.find("td", class_="MvTbTtl")
                 elem_json['title'] = info.a.get_text(strip=True)
-                lang, elem_json['title'] = clear_title(elem_json['title'])
-                """ Esto lo he implementado con etiquetas obtenidas de TMDB y una modificación en Unify. Algunas veces falla TMDB, pero debería de valer
-                nextChapterDateRegex = r'\s*-\s*(Proximo\s*Capitulo\s*\d+-[A-Za-z]+-\d+)'
-                if re.search(nextChapterDateRegex, elem_json['title']):
-                    nextChapterDate = scrapertools.find_single_match(elem_json['title'], nextChapterDateRegex)
-                    elem_json['title'] = re.sub(nextChapterDateRegex, '', elem_json['title'])
-                    elem_json['plot_extend'] = '[COLOR red][' + nextChapterDate + '][/COLOR]'
-                 """
 
                 elem_json['episode'] = int(elem.find("span", class_="Num").get_text(strip=True) or 1)
                 elem_json['url'] = info.a.get("href", "")
@@ -355,6 +273,12 @@ def episodesxseason_matches(item, matches_int, **AHkwargs):
 
             if not elem_json.get('url', ''): 
                 continue
+            
+            # Asi lee los datos correctos de TMDB
+            if elem_json['season'] == 1:
+                titleSeason = get_title_season(item.url)
+                if titleSeason != elem_json['season']:
+                    elem_json['season'] = titleSeason
 
             matches.append(elem_json.copy())
 
@@ -363,12 +287,6 @@ def episodesxseason_matches(item, matches_int, **AHkwargs):
 
 def findvideos(item, **AHkwargs):
     logger.info()
-    
-    """ No es necesario con la moficiación hecha en AH
-    # logger.info(item, True)
-    if item.contentType == 'movie':
-        return AlfaChannel.seasons(item, **kwargs)
-    """
 
     kwargs['matches_post_episodes'] = episodesxseason_matches
 
@@ -511,7 +429,7 @@ def check_hjstream(url):
 
     return url
 
-def clear_title(title):
+def get_lang_from_title(title):
 
     if 'latino' in title.lower():
         lang = 'Latino'
@@ -522,8 +440,19 @@ def clear_title(title):
     else:
         lang = 'VOSE'
 
-    title = re.sub(r'^[P|p]el[i|í]cula |HD|Español Castellano|Sub Español|Español Latino|ova\s+\d+:|OVA\s+\d+|\:|\((.*?)\)| \s19\d{2}| \s20\d{2}', '', title)
-    title = re.sub(r'\s:', ':', title)
-    title = " ".join( title.split() )
+    return lang
 
-    return lang, title
+# Algunas series tienen la temporada en el titulo, lo cual hace que TMDB devuelva los datos incorrectos
+# Ya que por defecto la temporada se obtiene de otro lado, esto crea una ambigüedad.
+# Esta funcion se usa para extraer el numero de temporada correcto del titulo
+def get_title_season(url):
+    logger.info()
+
+    page = AlfaChannel.create_soup(url, hide_infobox=True, **kwargs)
+    title = page.find("h1", class_="Title").get_text(strip=True)
+    season = 1
+    seasonPattern = '\s+Temporada\s+(\d+)'
+    if re.search(seasonPattern, title):
+        season = int(scrapertools.find_single_match(title, seasonPattern))
+
+    return season
