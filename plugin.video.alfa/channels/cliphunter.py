78b3a302dbc92569bf35734a8e11ed38a4a2a263
paeznet <jsaezu@wanadoo.es>
2024-01-18 13:12:25 +0100

    +18

--- plugin.video.alfa/channels/cliphunter.py
+++ plugin.video.alfa/channels/cliphunter.py
@@ -21,8 +21,8 @@ from core import httptools
 canonical = {
              'channel': 'cliphunter', 
              'host': config.get_setting("current_host", 'cliphunter', default=''), 
-             'host_alt': ["https://www.cliphunter.com/"], 
-             'host_black_list': [], 
+             'host_alt': ["https://cliphunter.com/"], 
+             'host_black_list': ["https://www.cliphunter.com/"], 
              'pattern': ['<link href="([^"]+)" rel="alternate"'], 
              'set_tls': True, 'set_tls_min': True, 'retries_cloudflare': 1, 'cf_assistant': False, 
              'CF': False, 'CF_test': False, 'alfa_s': True
