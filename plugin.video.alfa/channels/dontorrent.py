1c585466cd7e5db32eb48c7b0a6bb4c9bd1e28fd
Kingbox <37674310+lopezvg@users.noreply.github.com>
2024-07-04 13:38:01 +0200

    - Cambios de Dominio: Dontorrent

--- plugin.video.alfa/channels/dontorrent.py
+++ plugin.video.alfa/channels/dontorrent.py
@@ -25,9 +25,9 @@ forced_proxy_opt = 'ProxySSL'
 canonical = {
              'channel': 'dontorrent', 
              'host': config.get_setting("current_host", 'dontorrent', default=''), 
-             'host_alt': ["https://dontorrent.city/", "https://www11.dontorrent.link/", "https://tomadivx.net/",
+             'host_alt': ["https://dontorrent.cologne/", "https://www12.dontorrent.link/", "https://tomadivx.net/",
                           "https://todotorrents.org/"], 
-             'host_black_list': ["https://dontorrent.esq/", 
+             'host_black_list': ["https://dontorrent.city/", "https://dontorrent.esq/", 
                                  "https://dontorrent.cc/", "https://dontorrent.sbs/", "https://dontorrent.fyi/", 
                                  "https://dontorrent.icu/", "https://dontorrent.clothing/", "https://dontorrent.business/", 
                                  "https://dontorrent.miami/", "https://dontorrent.boutique/", "https://dontorrent.directory/", 
