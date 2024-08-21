583893e7c8103aa2ad1c21dce5feb0fbe06f030f
paeznet <jsaezu@wanadoo.es>
2024-08-21 09:45:16 +0200

    +18

--- plugin.video.alfa/servers/stormedia.py
+++ plugin.video.alfa/servers/stormedia.py
@@ -35,7 +35,8 @@ def get_video_url(page_url, video_password):
         # /cqlvid/  /wqlvid/
         # url = "https://s%s.fapmedia.com/wqpvid/%s/%s/%s/%s/%s_%s.mp4" %(server,s1,s2,id1,id,id,quality)
         # url = "https://s%s.stormedia.info/whpvid/%s/%s/%s/%s/%s_%s.mp4"   % (servidor, num, pal,vid, id,id, quality)
-        url = "https://%s.vstor.info/whpvid/%s/%s/%s/%s/%s_%s.mp4"   % (servidor, num, pal,vid, id,id, quality)
+        # url = "https://%s.vstor.info/whpvid/%s/%s/%s/%s/%s_%s.mp4"   % (servidor, num, pal,vid, id,id, quality)
+        url = "https://%s.vstor.top/whpvid/%s/%s/%s/%s/%s_%s.mp4"   % (servidor, num, pal,vid, id,id, quality)
         url = url.replace("_720p", "")
         video_urls.append(["[%s] %s" %(server,quality), url])
     video_urls.sort(key=lambda item: int( re.sub("\D", "", item[0])))
