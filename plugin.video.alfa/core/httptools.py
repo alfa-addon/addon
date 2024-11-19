179802d43e946b58099b8a2e0f0433846f080215
SistemaRayoXP <edsonarmando78@outlook.com>
2024-11-19 12:43:04 -0600

    Detalles extra

--- plugin.video.alfa/core/httptools.py
+++ plugin.video.alfa/core/httptools.py
@@ -1860,8 +1860,9 @@ def downloadpage(url, **opt):
                     else 1
                 )
                 if ssl_version and ssl:
-                    if ssl_context == ssl.PROTOCOL_TLS_CLIENT:
-                        opt["set_tls"] = ssl.PROTOCOL_TLSv1_2
+                    if PY3:
+                        if ssl_context == ssl.PROTOCOL_TLS_CLIENT:
+                            opt["set_tls"] = ssl.PROTOCOL_TLSv1_2
                     if ssl_context == ssl.PROTOCOL_TLSv1_2:
                         opt["set_tls"] = ssl.PROTOCOL_TLSv1_1
                     if ssl_context == ssl.PROTOCOL_TLSv1_1:
@@ -2210,14 +2211,14 @@ def show_infobox(info_dict, force=False):
     from textwrap import wrap
 
     box_items_kodi = {
-        "r_up_corner": "\u250c",
-        "l_up_corner": "\u2510",
-        "center": "\u2502",
-        "r_center": "\u251c",
-        "l_center": "\u2524",
-        "fill": "\u2500",
-        "r_dn_corner": "\u2514",
-        "l_dn_corner": "\u2518",
+        "r_up_corner": u"\u250c",
+        "l_up_corner": u"\u2510",
+        "center": u"\u2502",
+        "r_center": u"\u251c",
+        "l_center": u"\u2524",
+        "fill": u"\u2500",
+        "r_dn_corner": u"\u2514",
+        "l_dn_corner": u"\u2518",
     }
 
     box_items = {
