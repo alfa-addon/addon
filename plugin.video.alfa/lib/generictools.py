3255fd42654635ca6bd325d03b35ceac6f0c29ae
Kingbox <37674310+lopezvg@users.noreply.github.com>
2025-03-07 22:11:38 +0100

    - Mejora en BTDigg

--- plugin.video.alfa/lib/generictools.py
+++ plugin.video.alfa/lib/generictools.py
@@ -3386,6 +3386,9 @@ def CACHING_find_btdigg_list_all_NEWS_from_BTDIGG_(options=None):
                         continue
 
                     item, elem = set_tmdb_to_json(elem, title_search=title_search, title=title_save)
+                    if contentType == 'tvshow' and elem.get('tmdb_id', 'XXX') in str(cached[contentType]).lower():
+                        logger.debug('Error en TÍTULO DUP: %s - %s' % (elem['title'], elem.get('tmdb_id')))
+                        continue
 
                     cached[contentType].append(elem.copy())
 
