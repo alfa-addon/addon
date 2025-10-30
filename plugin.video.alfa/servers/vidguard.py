5a8ab42ce4105235fa6d0b35bfb048544cb602ed
DieFeM <danifmdelloret@hotmail.com>
2025-10-31 03:48:01 +0400

    Vidguard fix

--- plugin.video.alfa/servers/vidguard.py
+++ plugin.video.alfa/servers/vidguard.py
@@ -4,8 +4,6 @@
 # --------------------------------------------------------
 
 import re
-
-from core import urlparse
 from core import httptools
 from platformcode import logger
 from lib import aadecode
@@ -13,41 +11,52 @@ import base64
 import binascii
 from core import jsontools
 
+# Código original de Gujal00, ResolveURL
+# https://github.com/Gujal00/ResolveURL/blob/d497c23a1cca21e17fe1e5a4bccf08063978910b/script.module.resolveurl/lib/resolveurl/plugins/vidguard.py#L38
 # https://vembed.net/e/MAlwEMZea4OJ39X  #360 720 1080
 
 def test_video_exists(page_url):
+    global json_data
     logger.info("(page_url='%s')" % page_url)
     data = httptools.downloadpage(page_url).data
-    if "<h2>WE ARE SORRY</h2>" in data or 'Not Found' in data or 'The requested video was not found.' in data:
-        return False, "[4tube] El fichero no existe o ha sido borrado"
+    
+    r = re.search(r'eval\("window\.ADBLOCKER\s*=\s*false;\\n(.+?);"\);</script', data)
+    if not r:
+        return False, "[vidguard] No se ha podido encontrar el video"
+    
+    r = r.group(1).replace('\\u002b', '+')
+    r = r.replace('\\u0027', "'")
+    r = r.replace('\\u0022', '"')
+    r = r.replace('\\/', '/')
+    r = r.replace('\\\\', '\\')
+    r = r.replace('\\"', '"')
+    
+    text_decode = aadecode.decode(r, alt=True)
+    if "p,a,c,k,e,r" in text_decode:
+        return False, "[vidguard] Video is being encode"
+
+    try:
+        js_data = text_decode[11:]
+        json_data = jsontools.load(js_data)  #quita window.svg=
+    except Exception as e:
+        logger.error("Error loading JSON data: %s", e)
+    
+    if not json_data or not json_data.get('stream'):
+        return False, "[vidguard] No se ha podido encontrar la URL del video"
+    
     return True, ""
 
 
 def get_video_url(page_url, video_password):
+    global json_data
     logger.info("(page_url='%s')" % page_url)
     video_urls = []
-    data = httptools.downloadpage(page_url).data
-    r = re.search(r'eval\("window\.ADBLOCKER\s*=\s*false;\\n(.+?);"\);</script', data)
-    if r:
-        r = r.group(1).replace('\\u002b', '+')
-        r = r.replace('\\u0027', "'")
-        r = r.replace('\\u0022', '"')
-        r = r.replace('\\/', '/')
-        r = r.replace('\\\\', '\\')
-        r = r.replace('\\"', '"')
-        text_decode = aadecode.decode(r, alt=True)
-        json_data = jsontools.load(text_decode[11:])  #quita window.svg=
-        url = json_data['stream']
-        if url:
-            if isinstance(url, list):
-                sources = [(x.get('Label'), x.get('URL')) for x in url]
-                stream_url = helpers.pick_source(helpers.sort_sources_list(sources))
-            if not url.startswith('https://'):
-                url = re.sub(':/*', '://', stream_url)
-            url = sig_decode(url)
-            url += "|Referer=%s" % page_url
-            video_urls.append(["[vidguard]", url])
-        # video_urls.sort(key=lambda item: int( re.sub("\D", "", item[0])))
+    url = json_data['stream']
+    if not url.startswith('https://'):
+        url = re.sub(':/*', '://', url)
+    url = sig_decode(url)
+    url += "|Referer=%s" % page_url
+    video_urls.append(["[vidguard]", url])
     return video_urls
 
 
