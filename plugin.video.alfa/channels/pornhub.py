20c97136ecf85e7d655596088a99a06c1bcc4248
paeznet <jsaezu@wanadoo.es>
2026-08-06 09:16:29 +0200

    servers

--- plugin.video.alfa/channels/pornhub.py
+++ plugin.video.alfa/channels/pornhub.py
@@ -26,8 +26,11 @@ canonical = {
              'host': config.get_setting("current_host", 'pornhub', default=''), 
              'host_alt': ["https://es.pornhub.com/"], 
              'host_black_list': [], 
-             'set_tls': True, 'set_tls_min': True, 'retries_cloudflare': 1, 'forced_proxy_ifnot_assistant': forced_proxy_opt, 
+             'set_tls': None, 'set_tls_min': False, 'retries_cloudflare': 5, 'forced_proxy_ifnot_assistant': forced_proxy_opt, 
+             'cf_assistant': False, 'CF_stat': True, 
              'CF': False, 'CF_test': False, 'alfa_s': True
+             # 'set_tls': True, 'set_tls_min': True, 'retries_cloudflare': 1, 'forced_proxy_ifnot_assistant': forced_proxy_opt, 
+             # 'CF': False, 'CF_test': False, 'alfa_s': True
             }
 
 host = canonical['host'] or canonical['host_alt'][0]
